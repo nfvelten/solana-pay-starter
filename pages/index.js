415cdc7b6ee7ae649746db7fea5dd9b6f98a1491
Nicholas Velten <nikvelten@gmail.com>
2023-07-19 16:26:08 -0300

    feat: start on solana pay

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,86 @@
-import React from "react";
-import HeadComponent from '../components/Head';
+import React, { useState, useEffect } from "react";
+import CreateProduct from "../components/CreateProduct";
+import Product from "../components/Product";
+import HeadComponent from "../components/Head";
+
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 // Constants
 const TWITTER_HANDLE = "web3dev_";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
-  
+  const { publicKey } = useWallet();
+  const isOwner = publicKey
+    ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY
+    : false;
+  const [creating, setCreating] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then((response) => response.json())
+        .then((data) => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img
+        src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3h6MW9yMWNsN2Y4cnN4d213cTBycTU4YjJmc3E5amY1OWIxYnh0MiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/rTpONrw7mgrdg1Y97t/giphy.gif"
+        alt="emoji"
+      />
+
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>
+    </div>
+  );
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
   return (
     <div className="App">
-      <HeadComponent/>
+      <HeadComponent />
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Loja de emojis 😈</p>
-          <p className="sub-text">A única loja de emojis que aceita shitcoins</p>
+          <p className="header"> Beer Shop 🍺 </p>
+          <p className="sub-text">
+            A única loja de cerveja que aceita beertcoins
+          </p>
+
+          {isOwner && (
+            <button
+              className="create-product-button"
+              onClick={() => setCreating(!creating)}
+            >
+              {creating ? "Close" : "Criar Produto"}
+            </button>
+          )}
         </header>
 
         <main>
-          <img className='gif-image' src="https://media.tenor.com/1mJ-tJSzvwsAAAAd/solana-sol.gif" alt="emoji" />
+          {creating && <CreateProduct />}
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+          <img
+            alt="Twitter Logo"
+            className="twitter-logo"
+            src="twitter-logo.svg"
+          />
           <a
             className="footer-text"
             href={TWITTER_LINK}
